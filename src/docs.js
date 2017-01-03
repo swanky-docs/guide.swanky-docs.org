ab9400c3b3102d23de2232654661853912d0b546
Rod Leviton <rod@rodleviton.com>
2017-01-03 21:41:00 +1100

    add version number to guide

--- src/docs.js
+++ src/docs.js
@@ -2,5 +2,17 @@ if (__DEV__) {
   module.hot.accept();
 }
 
-// Documentation specific styles
 import './docs.styl';
+import GitHub from 'github-api'
+
+const gh = new GitHub()
+const repo = gh.getRepo('swanky-docs', 'swanky')
+
+repo.listReleases(function(err, res) {
+  if (!err) {
+    const tagHolder = document.getElementById('version')
+    const tag = res[0].tag_name
+
+    tagHolder.innerHTML = 'Swanky ' + tag
+  }
+})
